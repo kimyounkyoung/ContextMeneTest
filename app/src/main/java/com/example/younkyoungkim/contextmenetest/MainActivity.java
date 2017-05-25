107de6e97979298f77d4e8f58324b5308ffb9b0e
kimyounkyoung <passposs2@naver.com>
2017-05-25 19:53:16 +0900

    #4 event programming

--- app/src/main/java/com/example/younkyoungkim/contextmenetest/MainActivity.java
+++ app/src/main/java/com/example/younkyoungkim/contextmenetest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.younkyoungkim.contextmenetest;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -7,15 +8,18 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linear;
+    Button but2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but1 = (Button)findViewById(R.id.but1);
-        Button but2 = (Button)findViewById(R.id.but2);
+        but2 = (Button)findViewById(R.id.but2);
+        linear=(LinearLayout)findViewById(R.id.linear);
         registerForContextMenu(but1);
         registerForContextMenu(but2);
     }
@@ -25,11 +29,35 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         if(v.getId()==R.id.but1){
             menu.setHeaderTitle("배경색 변경");
-            // menu.setHeaderIcon(R.drawable.icon1);
+            menu.setHeaderIcon(R.drawable.icon1);
             menuInflater.inflate(R.menu.menu1,menu);
         }
         if(v.getId()==R.id.but2)
             menuInflater.inflate(R.menu.menu2,menu);
     }
 
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+        switch (item.getItemId()){
+            case R.id.menu1_item_red:
+                linear.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.menu1_item_blue:
+                linear.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.menu1_item_pink:
+                linear.setBackgroundColor(Color.rgb(255,178,217));
+                return true;
+            case R.id.menu2_item_rotation:
+                but2.setRotation(90);
+                return true;
+            case R.id.menu2_item_size:
+                but2.setScaleX(1.5f);
+                but2.setScaleY(1.5f);
+                return true;
+        }
+
+        return false;
+    }
 }
